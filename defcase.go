33f1521da10ec1bd4d612d2b9eaf3eb7acd5f66f
sherlock <abdullayev.13.dev@gmail.com>
2023-10-06 23:59:09 +0500

    65l done

--- defcase.go
+++ defcase.go
@@ -1,11 +1,9 @@
 package json
 
-type defCaseFn func(pkgPath, name string) string
-
 var defCase = func(pkgPath, name string) string {
 	return name
 }
 
-func DefCaseFn(f func(tag string) defCaseFn) {
+func DefCaseFn(f func(tag string) func(pkgPath, name string) string) {
 	defCase = f("json")
 }
